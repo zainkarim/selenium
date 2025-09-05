82f4d4ed088f666e47385726800f50f2b29814ba
Zain Karim <zain@zainkarim.com>
2025-09-04 22:19:35 -0500

    Initial Commit

---
+++ selenium/seleniumApp.swift
@@ -0,0 +1,32 @@
+//
+//  seleniumApp.swift
+//  selenium
+//
+//  Created by Zain Karim on 9/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct seleniumApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ seleniumTests/seleniumTests.swift
@@ -0,0 +1,17 @@
+//
+//  seleniumTests.swift
+//  seleniumTests
+//
+//  Created by Zain Karim on 9/4/25.
+//
+
+import Testing
+@testable import selenium
+
+struct seleniumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
